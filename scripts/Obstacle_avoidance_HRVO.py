b69c6a48c966e65503cdc9a53ceb43f9f1fc8a41
RugvedKatole <f20180391@goa.bits-pilani.ac.in>
2022-04-09 23:44:42 +0530

    1.25.2

--- scripts/Obstacle_avoidance_HRVO.py
+++ scripts/Obstacle_avoidance_HRVO.py
@@ -9,25 +9,29 @@ import math
 from tf.transformations import euler_from_quaternion
 import rospy
 from PID_control import PID_control
+from multi_robot_navigation.msg import Obs, ObsData
 
 class RobotHRVO(object):
     def __init__(self, no_of_robots):
         self.node_name = rospy.get_name()
+        self.namespace = rospy.get_namespace()
         rospy.loginfo("[%s] Initiazlizing " %self.node_name)
         self.total_bots = no_of_robots
         
         #pubs and subs
-        self.cmd_pub0 = rospy.Publisher("/tb3_0/cmd_vel",Twist,queue_size=1)
-        self.odom_sub0 =  rospy.Subscriber("/tb3_0/odom", Odometry, self.odom_update_0,queue_size=1)
+        self.obstacles = rospy.Subscriber('/obs_data',ObsData,self.update_obstacles)
+        self.cmd_vel = rospy.Publisher(self.namespace + "/cmd_vel",Twist,queue_size=1)
+        # self.cmd_pub0 = rospy.Publisher("/tb3_0/cmd_vel",Twist,queue_size=1)
+        # self.odom_sub0 =  rospy.Subscriber("/tb3_0/odom", Odometry, self.odom_update_0,queue_size=1)
 
-        self.cmd_pub1 = rospy.Publisher("/tb3_1/cmd_vel",Twist,queue_size=1)
-        self.odom_sub1 = rospy.Subscriber("/tb3_1/odom", Odometry, self.odom_update_1,queue_size=1)
+        # self.cmd_pub1 = rospy.Publisher("/tb3_1/cmd_vel",Twist,queue_size=1)
+        # self.odom_sub1 = rospy.Subscriber("/tb3_1/odom", Odometry, self.odom_update_1,queue_size=1)
         
-        self.cmd_pub2 = rospy.Publisher("/tb3_2/cmd_vel",Twist,queue_size=1)
-        self.odom_sub2 =  rospy.Subscriber("/tb3_2/odom", Odometry, self.odom_update_2,queue_size=1)
+        # self.cmd_pub2 = rospy.Publisher("/tb3_2/cmd_vel",Twist,queue_size=1)
+        # self.odom_sub2 =  rospy.Subscriber("/tb3_2/odom", Odometry, self.odom_update_2,queue_size=1)
 
-        self.cmd_pub3 = rospy.Publisher("/tb3_3/cmd_vel",Twist,queue_size=1)
-        self.odom_sub3 = rospy.Subscriber("/tb3_3/odom", Odometry, self.odom_update_3,queue_size=1)
+        # self.cmd_pub3 = rospy.Publisher("/tb3_3/cmd_vel",Twist,queue_size=1)
+        # self.odom_sub3 = rospy.Subscriber("/tb3_3/odom", Odometry, self.odom_update_3,queue_size=1)
 
 
         
@@ -37,7 +41,7 @@ class RobotHRVO(object):
         self.yaw = [0 for i in range(self.total_bots)]
 
         #initializing PID (tracking control law)
-        self.PID = [PID_control("bot_%i" %i,publisher_name="/tb3_%i/cmd_vel" %i,odom_name= "/tb3_%i/odom" %i) for i in range(self.total_bots)]
+        self.PID = PID_control(self.namespace,publisher_name = self.namespace + "/cmd_vel",odom_name =self.namespace + "/odom")
 
         
         #initializing HRVI env
@@ -60,7 +64,7 @@ class RobotHRVO(object):
 
         self.delta_t = 0.2
 
-        self.timer = rospy.Timer(rospy.Duration(0.02),self.hrvo)
+        self.timer = rospy.Timer(rospy.Duration(self.delta_t),self.hrvo)
 
     def hrvo(self, event):
         self.update_all()
@@ -68,9 +72,8 @@ class RobotHRVO(object):
         self.velocity = RVO_update(self.position, v_des, self.velocity_detect,self.ws_model)
         # print(self.velocity)
 
-        for i in range(self.total_bots):
             # param_point = self.point_generator(self.velocity[i][0],self.velocity[i][0])
-            self.PID[i].Velocity_tracking_law(self.velocity[i][0],self.velocity[i][1])
+        self.PID.Velocity_tracking_law(self.velocity[0][0],self.velocity[0][1])
             # print(self.PID[i].name)
 
     # def point_generator(self,vx,vy):
@@ -81,22 +84,32 @@ class RobotHRVO(object):
     #     print(param_points)
     #     return param_points
 
-    def process_ang_dis(self,vx,vy,yaw):
-        dest_yaw = math.atan2(vy,vx)
-        if dest_yaw > yaw:
-            right_yaw = dest_yaw-yaw
-            left_yaw = (2*math.pi-right_yaw)*-1
-        else:
-            left_yaw = dest_yaw-yaw
-            right_yaw = (2*math.pi-(left_yaw*-1))
-        #-1 < angle < 1 , find close side to turn
-        angle = left_yaw if abs(left_yaw) < abs(right_yaw) else right_yaw
-        angle = angle/math.pi
-        dis = (((1 - abs(angle))-0.5) * 2) * pow(vx*vx + vy*vy,0.5)
-        # what is the first term in above equation ??
-        dis = max(min(dis,1),-1)
-        angle = max(min(angle,1),0)
-        return dis , angle
+    # def process_ang_dis(self,vx,vy,yaw):
+    #     dest_yaw = math.atan2(vy,vx)
+    #     if dest_yaw > yaw:
+    #         right_yaw = dest_yaw-yaw
+    #         left_yaw = (2*math.pi-right_yaw)*-1
+    #     else:
+    #         left_yaw = dest_yaw-yaw
+    #         right_yaw = (2*math.pi-(left_yaw*-1))
+    #     #-1 < angle < 1 , find close side to turn
+    #     angle = left_yaw if abs(left_yaw) < abs(right_yaw) else right_yaw
+    #     angle = angle/math.pi
+    #     dis = (((1 - abs(angle))-0.5) * 2) * pow(vx*vx + vy*vy,0.5)
+    #     # what is the first term in above equation ??
+    #     dis = max(min(dis,1),-1)
+    #     angle = max(min(angle,1),0)
+    #     return dis , angle
+
+
+    def update_obstacles(self,data):
+        bot_id = data.bot_id
+        odoms = data.obstacles
+        cur_bot_id_indx = bot_id.index(self.namespace)
+        bot_odom = odoms.pop(cur_bot_id_indx)
+        odoms.insert(0,bot_odom)
+        self.bot_odom = odoms
+
 
     def update_all(self):
         self.position = []
---
+++ scripts/obstacles.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import rospy
+import math
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from multi_robot_navigation.msg import ObsData, Obs
+from tf.transformations import euler_from_quaternion
+
+
+class Obstacles:
+
+    def __init__(self):
+
+        self.obs_pub = rospy.Publisher('/obs_data', ObsData, queue_size = 10)
+        rospy.Subscriber('/tb3_0/odom', Odometry, self.callback_odom, 'tb3_0')
+        rospy.Subscriber('/tb3_1/odom', Odometry, self.callback_odom, 'tb3_1')
+        rospy.Subscriber('/tb3_2/odom', Odometry, self.callback_odom, 'tb3_2')
+        rospy.Subscriber('/tb3_3/odom', Odometry, self.callback_odom, 'tb3_3')
+        self.obs = {}
+        self.obs['tb3_0'] = Odometry()
+        self.obs['tb3_1'] = Odometry()
+        self.obs['tb3_2'] = Odometry()
+        self.obs['tb3_3'] = Odometry()
+
+    def callback_odom(self, data, bot_id):
+        self.obs[bot_id] = data
+        # self.obs[bot_id].obs = bot_id
+        # self.obs[bot_id].pose_x = data.pose.pose.position.x
+        # self.obs[bot_id].pose_y = data.pose.pose.position.y
+        # orient = data.pose.pose.orientation
+        # (roll, pitch, yaw) = euler_from_quaternion([orient.x, orient.y, orient.z, orient.w])
+        # if yaw < 0:
+        #     yaw += 2 * math.pi
+        # # self.obs[bot_id].pose.theta = yaw
+
+        # lin_vel = data.twist.twist.linear.x
+        # self.obs[bot_id].vel_x = lin_vel * math.cos(yaw)
+        # self.obs[bot_id].vel_y = lin_vel * math.sin(yaw)
+
+if __name__ == '__main__':
+    rospy.init_node('collision_cone_obstacles', anonymous = True)
+    s = Obstacles()
+    r = rospy.Rate(30)
+    while not rospy.is_shutdown():
+        obs = []
+        bot_id = []
+        for i, n in s.obs.items():
+            obs.append(n)
+            bot_id.append(i)
+
+        obs_data = ObsData()
+        obs_data.obstacles = obs
+        obs_data.bot_id = bot_id
+        print(obs_data)
+        s.obs_pub.publish(obs_data)
+        r.sleep() 
